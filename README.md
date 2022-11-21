16426a247a4e221c6f19299603b9c70b1b95d900
zgamble30 <you@example.com>
2022-11-21 10:38:33 -0800

    task 1

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-this is a readme
+# Monty
+
+Monty 0.98 is a scripting language that is first compiled into Monty byte codes (Just like Python). It relies on a unique stack, with specific instructions to manipulate it. The goal of this project is to create an interpreter for Monty ByteCodes files.
+
+## Monty byte code files
+
+Files containing Monty byte codes usually have the .m extension. Most of the industry uses this standard but it is not required by the specification of the language. There is not more than one instruction per line. There can be any number of spaces before or after the opcode and its argument:
+
+```
+ push 0$
+push 1$
+push 2$
+  push 3$
+                   pall    $
+push 4$
+    push 5    $
+      push    6        $
+pall$
+
+```
+
+Monty byte code files can contain blank lines (empty or made of spaces only, and any additional text after the opcode or its required argument is not taken into account:
+
+```
+push 0 Push 0 onto the stack$
+push 1 Push 1 onto the stack$
+$
+push 2$
+  push 3$
+                   pall    $
+$
+$
+                           $
+push 4$
+$
+    push 5    $
+      push    6        $
+$
+pall This is the end of our program. Monty is awesome!$
+
+```
+
+# Usage
+
+All the files are compiled in the following form:
+
+```
+ gcc -Wall -Werror -Wextra -pedantic *.c -o monty.
+
+```
+
+To Run the program:
+
+```
+ ./monty bytecode_file
+```
+
+Available Operation Codes:
+
+| Opcode | Description |
+|---------------- | -----------|
+|push   | Pushes an element to the stack. e.g (push 1 # pushes 1 into the stack)|
+|pall   | Prints all the values on the stack, starting from the to of the stack.|
+|pint   | Prints the value at the top of the stack.|
+|pop    | Removes the to element of the stack. |
+|swap   | Swaps the top to elements of the stack.|
+|add    | Adds the top two elements of the stack. The result is then stored in the second node, and the first node is removed.|
+|nop    | This opcode does not do anything.|
--- monty.h
+++ monty.h
@@ -1,30 +1,14 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
-
-#define INSTRUCTIONS              \
-	{                           \
-		{"push", push},       \
-		    {"pall", pall},   \
-		    {"pint", pint},   \
-		    {"pop", pop},     \
-		    {"swap", swap},   \
-		    {"nop", nop},     \
-		    {"div", _div},    \
-		    {"mul", _mul},    \
-		    {"add", _add},    \
-		    {"sub", _sub},    \
-		    {"mod", mod},     \
-		    {"pchar", pchar}, \
-		    {"pstr", pstr},   \
-		    {"rotl", rotl},   \
-		    {"rotr", rotr},   \
-		{                     \
-			NULL, NULL      \
-		}                     \
-	}
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -33,7 +17,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
@@ -48,7 +32,7 @@ typedef struct stack_s
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
@@ -57,37 +41,41 @@ typedef struct instruction_s
 } instruction_t;
 
 /**
-* struct help - argument for the current opcode
-* @data_struct: stack mode, stack (default) and queue
-* @argument: the arguments of the string
-*
-* Description: global structure used to pass data around the functions easily
-*/
-typedef struct help
+ * struct glob_s - global and its funcs
+ * @fd: File descriptor
+ * @line: Line to getline
+ *
+ * Description: To handle the file and getline
+ */
+typedef struct glob_s
 {
-	int data_struct;
-	char *argument;
-} help;
-help global;
+	FILE *fd;
+	char *line;
+} glob_t;
 
-/* stack utility functions available in linked_list.c */
-stack_t *add_node(stack_t **stack, const int n);
-stack_t *queue_node(stack_t **stack, const int n);
-void free_stack(stack_t *stack);
-size_t print_stack(const stack_t *stack);
+extern glob_t global;
+extern int value;
 
-void push(stack_t **stack, unsigned int line_cnt);
-void pall(stack_t **stack, unsigned int line_cnt);
-void pint(stack_t **stack, unsigned int line_cnt);
-void swap(stack_t **stack, unsigned int line_cnt);
-void pop(stack_t **stack, unsigned int line_cnt);
-void nop(stack_t **stack, unsigned int line_cnt);
+void handle_command(char *argv);
 
-void _add(stack_t **stack, unsigned int line_cnt);
+int get_opc(stack_t **stack, char *arg, char *item, int count);
 
-void opcode(stack_t **stack, char *str, unsigned int line_cnt);
+void _push(stack_t **stack, unsigned int line_number);
+void _pall(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
+void cleanStack(stack_t **stack);
+void free_dlistint(stack_t *stack);
 
-int is_digit(char *string);
-int isnumber(char *str);
+/*Help*/
+int _isdigit(char *c);
+stack_t *new_Node(int n);
 
-#endif /* MONTY_H */
+/* handle_errors */
+void push_error(FILE *fd, char *line, stack_t *stack, int count);
+void ins_error(FILE *fd, char *line, stack_t *stack, char *count, int item);
+
+#endif
